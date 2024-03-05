61c05f654f945fa87e14705900d93077f5e1d7e2
Alex Morra <144852352+amorra1@users.noreply.github.com>
2024-03-05 10:37:10 -0500

    question 4

--- main.py
+++ main.py
@@ -27,3 +27,13 @@ for i in range(0, 13):
     
 fig.tight_layout()
 plt.show()
+
+#Question 4
+dataset = pd.read_csv("heart.csv")
+data = dataset.iloc[:, 0:-1]
+labels = dataset.iloc[:, -1]
+fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
+data.plot(ax=ax.flatten()[:13], kind='density', subplots=True, sharex=False)
+fig.tight_layout()
+plt.show()
+
