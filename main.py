d72eb97650bbad29d25086e5bb50e62be8cbe38b
Alex Morra <144852352+amorra1@users.noreply.github.com>
2024-03-05 00:58:30 -0500

    question 3

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ data.hist(ax=ax.flatten()[:13])
 
 fig.tight_layout()
 plt.show()
+
+#Question 3
+dataset = pd.read_csv("heart.csv")
+data = dataset.iloc[:, 0:-1]
+labels = dataset.iloc[:, -1]
+fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20,10))
+
+for i in range(0, 13):
+    ax.flatten()[i].hist(data.iloc[:, i])
+    ax.flatten()[i].set_title(data.columns[i], fontsize=15)
+    
+fig.tight_layout()
+plt.show()
