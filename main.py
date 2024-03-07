70ed27aa34cab19b8a79f181f25b24d88f67ff8c
Alex Morra <144852352+amorra1@users.noreply.github.com>
2024-03-07 12:42:22 -0500

    Question 8&9

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 #Question 1
 dataset = pd.read_csv("heart.csv")
@@ -52,10 +55,63 @@ plt.show()
 dataset = pd.read_csv("heart.csv")
 data = dataset.iloc[:, 0:-1]
 labels = dataset.iloc[:, -1]
-fig, ax = plt.subplots(ncols=13, nrows=13, figsize=(30, 30))
+fig, ax = plt.subplots(ncols=13, nrows=13, figsize=(20, 20))
 
-pd.plotting.scatter_matrix(data, ax=ax.flatten().reshape(13, 13))
+pd.plotting.scatter_matrix(data, ax=ax)
 
 fig.tight_layout()
 plt.show()
 
+#Question 8
+df = pd.read_csv('wineQualityN.csv')
+
+df.drop(df.columns[0], axis=1, inplace=True)
+
+df['quality'] = df['quality'].apply(lambda x: 1 if x>= 8 else 0)
+features = df.drop('quality', axis=1)
+features = StandardScaler().fit_transform(features)
+
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(features)
+tsne = TSNE(n_components=2, perplexity=30)
+tsne_result = tsne.fit_transform(features)
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1,2,1)
+plt.scatter(pca_result[df['quality'] == 0, 0], pca_result[df['quality'] == 0, 1], c='pink', label='Low quality')
+plt.scatter(pca_result[df['quality'] == 1, 0], pca_result[df['quality'] == 1, 1], c='red', label='High Quality')
+plt.title('PCA')
+plt.xlabel('Principle Compoment 1')
+plt.ylabel('Principle Compoment 2') 
+plt.legend()
+plt.subplot(1,2,2)
+plt.scatter(tsne_result[df['quality'] == 0, 0], tsne_result[df['quality'] == 0, 1], c='pink', label='Low quality')
+plt.scatter(tsne_result[df['quality'] == 1, 0], tsne_result[df['quality'] == 1, 1], c='red', label='High Quality')
+
+plt.title('t-SNE')
+plt.xlabel('t-SNE Dimension 1')
+plt.ylabel('t-SNE Dimension 2')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+#Question 9
+df = pd.read_csv('wineQualityN.csv')
+df.drop(df.columns[0], axis=1, inplace=True)
+
+df['quality'] = df['quality'].apply(lambda x: 1 if x>= 8 else 0)
+features = df.drop('quality', axis=1)
+features = StandardScaler().fit_transform(features)
+
+pca = PCA(n_components=11)
+pca_result = pca.fit_transform(features)   
+
+plt.figure(figsize=(8, 6))
+plt.scatter(pca_result[df['quality'] == 0, 7], pca_result[df['quality'] == 0, 8], c='pink', label='Low quality')
+plt.scatter(pca_result[df['quality'] == 1, 7], pca_result[df['quality'] == 1, 8], c='red', label='High Quality')
+plt.title('PCA: Principal Component 8 vs Principal Component 9')
+plt.xlabel('Principle Compoment 8')
+plt.ylabel('Principle Compoment 9')
+plt.legend()
+plt.show()
